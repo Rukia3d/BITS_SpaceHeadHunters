857091f790915abba185418463ae1a98bc0e9c94
Kevin Murphy <s3407899@student.rmit.edu.au>
2017-09-16 15:40:13 +1000

    Bare-bones window app with icons and packaging scripts.

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # BITS_SpaceHeadHunters
 A study project for RMIT
+
+##Installation
+- Clone the repository.
+- Install dependencies with the "npm install" command.
+
+##Package Creation
+- Use the command for your OS;
+	- npm run package-win
+	- npm run package-mac
+	- npm run package-linux
---
+++ main.js
@@ -0,0 +1,30 @@
+// includes
+var electron = require("electron");
+var path = require("path");
+
+var BrowserWindow = electron.BrowserWindow;
+var app = electron.app;
+var ipc = electron.ipcMain;
+
+app.on("ready", function() {
+
+	var appWindow = new BrowserWindow({
+
+		width: 1024,
+		height: 768,
+		icon: path.join(__dirname, 'assets/icons/png/64x64.png'),
+		show: false
+	});
+
+	appWindow.loadURL("file://" + __dirname + "/index.html");
+
+	appWindow.once("ready-to-show", function() {
+
+		// do we want to run it maximized?
+		appWindow.maximize();
+		appWindow.show();
+
+	});
+});
+
+app.on("window-all-closed", app.quit);
